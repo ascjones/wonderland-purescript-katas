375123f19baeb7fca6c884aca93c22bd5bb276b9
Andrew Jones <ascjones@gmail.com>
2016-05-08 12:04:00 +0100

    Replace brackets with $ in tests

--- AlphabetCipher/test/Main.purs
+++ AlphabetCipher/test/Main.purs
@@ -15,12 +15,12 @@ main = runTest do
   suite "encode" do
     test "can encode given a secret keyword" do
       Assert.equal "hmkbxebpxpmyllyrxiiqtoltfgzzv" $ encode "vigilance" "meetmeontuesdayeveningatseven"
-      Assert.equal "egsgqwtahuiljgs" (encode "scones" "meetmebythetree")
+      Assert.equal "egsgqwtahuiljgs" $ encode "scones" "meetmebythetree"
   suite "decode" do
     test "can decode an encyrpted message given a secret keyword" do
-      Assert.equal "meetmeontuesdayeveningatseven" (decode "vigilance" "hmkbxebpxpmyllyrxiiqtoltfgzzv")
-      Assert.equal "meetmebythetree" (decode "scones" "egsgqwtahuiljgs")
+      Assert.equal "meetmeontuesdayeveningatseven" $ decode "vigilance" "hmkbxebpxpmyllyrxiiqtoltfgzzv"
+      Assert.equal "meetmebythetree" $ decode "scones" "egsgqwtahuiljgs"
   suite "decipher" do
     test "can extract the secret keywork given an encrypted message and the original message" do
-      Assert.equal "vigilance" (decipher "opkyfipmfmwcvqoklyhxywgeecpvhelzg" "thequickbrownfoxjumpsoveralazydog")
-      Assert.equal "scones" (decipher "hcqxqqtqljmlzhwiivgbsapaiwcenmyu" "hcqxqqtqljmlzhwiivgbsapaiwcenmyu")
+      Assert.equal "vigilance" $ decipher "opkyfipmfmwcvqoklyhxywgeecpvhelzg" "thequickbrownfoxjumpsoveralazydog"
+      Assert.equal "scones" $ decipher "hcqxqqtqljmlzhwiivgbsapaiwcenmyu" "hcqxqqtqljmlzhwiivgbsapaiwcenmyu"
