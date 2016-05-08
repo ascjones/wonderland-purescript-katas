1add28a89523ef176064fcce7dc8b242ad166ac9
Andrew Jones <ascjones@gmail.com>
2016-05-08 13:00:27 +0100

    WIP: AlphabetCipher colution

--- AlphabetCipher/src/AlphabetCipher.purs
+++ AlphabetCipher/src/AlphabetCipher.purs
@@ -8,13 +8,18 @@ module AlphabetCipher
   ) where
 
 import Prelude
+import Data.Array (zip, range)
+import Data.String (toCharArray, fromCharArray, length)
+import Data.Tuple (uncurry)
 
 type SecretKey = String
 type PlainText = String
 type CipherText = String
 
 encode :: SecretKey -> PlainText -> CipherText
-encode key msg = "encodeme"
+encode key msg = fromCharArray $ (uncurry encodeChar) <$> zip (toCharArray msg) (range 0 $ length msg - 1)
+  where
+  encodeChar c i = c
 
 decode :: SecretKey -> CipherText -> PlainText
 decode key ct = "decodeme"
