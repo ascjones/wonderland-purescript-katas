cb8ad2443890a6cf9d785292ba6bd5da67d70ba0
Andrew Jones <ascjones@gmail.com>
2016-05-08 11:48:36 +0100

    Add AlphabetCipher tests

---
+++ AlphabetCipher/src/AlphabetCipher.purs
@@ -0,0 +1,16 @@
+module AlphabetCipher where
+
+import Prelude
+
+type SecretKey = String
+type PlainText = String
+type CipherText = String
+
+encode :: SecretKey -> PlainText -> CipherText
+encode key msg = "encodeme"
+
+decode :: SecretKey -> CipherText -> PlainText
+decode key ct = "decodeme"
+
+decipher :: CipherText -> PlainText -> SecretKey
+decipher ct msg = "decipherme"
---
+++ AlphabetCipher/test/Main.purs
@@ -0,0 +1,26 @@
+module Test.Main where
+
+import Prelude
+import Control.Monad.Eff (Eff)
+import Control.Monad.Eff.Console (CONSOLE)
+import Test.Unit.Assert as Assert
+import Test.Unit (test, suite)
+import Test.Unit.Console (TESTOUTPUT)
+import Test.Unit.Main (runTest)
+
+import AlphabetCipher (encode, decode, decipher)
+
+main :: forall eff. Eff (console :: CONSOLE, testOutput :: TESTOUTPUT | eff) Unit
+main = runTest do
+  suite "encode" do
+    test "can encode given a secret keyword" do
+      Assert.equal "hmkbxebpxpmyllyrxiiqtoltfgzzv" $ encode "vigilance" "meetmeontuesdayeveningatseven"
+      Assert.equal "egsgqwtahuiljgs" (encode "scones" "meetmebythetree")
+  suite "decode" do
+    test "can decode an encyrpted message given a secret keyword" do
+      Assert.equal "meetmeontuesdayeveningatseven" (decode "vigilance" "hmkbxebpxpmyllyrxiiqtoltfgzzv")
+      Assert.equal "meetmebythetree" (decode "scones" "egsgqwtahuiljgs")
+  suite "decipher" do
+    test "can extract the secret keywork given an encrypted message and the original message" do
+      Assert.equal "vigilance" (decipher "opkyfipmfmwcvqoklyhxywgeecpvhelzg" "thequickbrownfoxjumpsoveralazydog")
+      Assert.equal "scones" (decipher "hcqxqqtqljmlzhwiivgbsapaiwcenmyu" "hcqxqqtqljmlzhwiivgbsapaiwcenmyu")
