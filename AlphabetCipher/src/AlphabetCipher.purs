e557e7a20da6ce33966964d6cf2278cf78055d84
Andrew Jones <ascjones@gmail.com>
2016-05-08 20:20:30 +0100

    Half working encode

--- AlphabetCipher/src/AlphabetCipher.purs
+++ AlphabetCipher/src/AlphabetCipher.purs
@@ -9,17 +9,36 @@ module AlphabetCipher
 
 import Prelude
 import Data.Array (zip, range)
-import Data.String (toCharArray, fromCharArray, length)
+import Data.Char (toCharCode, toLower, fromCharCode)
+import Data.String (length, toCharArray, fromCharArray)
+import Data.String.Unsafe (charAt)
 import Data.Tuple (uncurry)
 
 type SecretKey = String
 type PlainText = String
 type CipherText = String
 
+charToInt :: Char -> Int
+charToInt = toLower >>> toCharCode
+
+a :: Int
+a = toCharCode 'a'
+
+z :: Int
+z = toCharCode 'z'
+
+substitute :: Char -> Char -> Char
+substitute keyChar msgChar =
+  let keyInt = charToInt keyChar in
+  let msgInt = charToInt msgChar in
+  fromCharCode $ (+) a $ mod (keyInt + msgInt) a
+
 encode :: SecretKey -> PlainText -> CipherText
 encode key msg = fromCharArray $ (uncurry encodeChar) <$> zip (toCharArray msg) (range 0 $ length msg - 1)
   where
-  encodeChar c i = c
+  encodeChar c i =
+    let keyChar = charAt (mod i $ length key) key in
+    substitute keyChar c
 
 decode :: SecretKey -> CipherText -> PlainText
 decode key ct = "decodeme"
