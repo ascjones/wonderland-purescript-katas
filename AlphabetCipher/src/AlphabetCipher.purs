d9935c0a136d89ef23efa7d6d98ee39d54d31e7f
Andrew Jones <ascjones@gmail.com>
2016-05-13 08:38:59 +0100

    Naive inplementation of decode

--- AlphabetCipher/src/AlphabetCipher.purs
+++ AlphabetCipher/src/AlphabetCipher.purs
@@ -21,10 +21,25 @@ type CipherText = String
 a :: Int
 a = toCharCode 'a'
 
+z :: Int
+z = toCharCode 'z'
+
 charToInt :: Char -> Int
 charToInt = toLower >>> toCharCode >>> toZeroBased
-  where
-  toZeroBased ch = ch - a
+  where toZeroBased ch = ch - a
+
+abs :: Int -> Int
+abs i = if i > 0 then i else i * -1
+
+--   0 1 2 3
+-- 0 0 1 2 3
+-- 1 1 2 3 0
+-- 2 2 3 0 1
+-- 3 3 0 1 2
+
+-- 0 1 2 3 0 1 2 3
+-- 3 - 2 = 1
+-- 2 - 3 = -1
 
 substitute :: Char -> Char -> Char
 substitute keyChar msgChar =
@@ -40,7 +55,14 @@ encode key msg = fromCharArray $ (uncurry encodeChar) <$> zip (toCharArray msg) 
     substitute keyChar c
 
 decode :: SecretKey -> CipherText -> PlainText
-decode key ct = "decodeme"
+decode key ct = fromCharArray $ (uncurry decodeChar) <$> zip (toCharArray ct) (range 0 $ length ct - 1)
+  where
+  decodeChar c i =
+    let keyChar = charAt (mod i $ length key) key in
+    let keyInt = charToInt keyChar in
+    let msgInt = charToInt c in
+    let diff = msgInt - keyInt in
+    fromCharCode $ (+) a $ if diff >= 0 then diff else diff + 26 -- todo make this function better
 
 decipher :: CipherText -> PlainText -> SecretKey
 decipher ct msg = "decipherme"
