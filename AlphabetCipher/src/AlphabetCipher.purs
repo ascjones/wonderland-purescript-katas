8f0887840ecc24facd82760fd3c789d11cb976b2
Andrew Jones <ascjones@gmail.com>
2016-05-31 17:45:21 +0100

    Refactor to extract common function for encode/decode

--- AlphabetCipher/src/AlphabetCipher.purs
+++ AlphabetCipher/src/AlphabetCipher.purs
@@ -34,31 +34,26 @@ charToInt = toLower >>> toCharCode >>> toZeroBased
 -- 2 2 3 0 1
 -- 3 3 0 1 2
 
-substitute :: Char -> Char -> Char
-substitute keyChar msgChar =
-  let keyInt = charToInt keyChar in
-  let msgInt = charToInt msgChar in
-  fromCharCode $ (+) a $ mod (keyInt + msgInt) 26
-
-mapChars :: (Char -> Char -> Char) -> SecretKey -> String -> String
+mapChars :: (Int -> Int -> Char) -> SecretKey -> String -> String
 mapChars f key msg = fromCharArray $ (uncurry f) <$> mapWithKeyChars
   where
   mapWithKeyChars =
-    let keyChars = (\i -> charAt (mod i $ length key) key) <$> (range 0 $ length msg -1) in
-    zip (toCharArray msg) keyChars
+    let keyChars = (\i -> charToInt $ charAt (mod i $ length key) key) <$> (range 0 $ length msg -1) in
+    zip (charToInt <$> toCharArray msg) keyChars
 
 encode :: SecretKey -> PlainText -> CipherText
-encode = mapChars substitute
+encode = mapChars encodeChar
+  where
+  encodeChar :: Int -> Int -> Char
+  encodeChar keyInt msgInt =
+    fromCharCode $ (+) a $ mod (keyInt + msgInt) 26
 
 decode :: SecretKey -> CipherText -> PlainText
-decode key ct = fromCharArray $ (uncurry decodeChar) <$> zip (toCharArray ct) (range 0 $ length ct - 1)
+decode = mapChars decodeChar
   where
-  decodeChar c i =
-    let keyChar = charAt (mod i $ length key) key in
-    let keyInt = charToInt keyChar in
-    let msgInt = charToInt c in
-    let diff = msgInt - keyInt in
-    fromCharCode $ (+) a $ mod (diff + 26) 26 -- todo make this function better
+  decodeChar keyInt msgInt =
+    let diff = keyInt - msgInt in
+    fromCharCode $ (+) a $ mod (diff + 26) 26
 
 decipher :: CipherText -> PlainText -> SecretKey
 decipher ct msg = "decipherme"
